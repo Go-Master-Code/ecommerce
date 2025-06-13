7a55e8a8c3cc1e08d2dfd6284ff65e4cfcfa7d2f
Go-Master-Code <kevinkristansa@gmail.com>
2025-06-13 08:18:51 +0700

    Update barang.go

--- models/barang.go
+++ models/barang.go
@@ -11,7 +11,7 @@ type Barang struct {
 	IdKategori int            `gorm:"column:id_kategori"`
 	NamaBarang string         `gorm:"column:nama_barang"`
 	Harga      float64        `gorm:"column:harga"`
-	Stok       int            `gorm:"column:stok"`
+	Stok       float64        `gorm:"column:stok"`
 	Deskripsi  string         `gorm:"column:deskripsi"`
 	ImagePath  string         `gorm:"column:image_path"`
 	CreatedAt  time.Time      `gorm:"column:created_at;autoCreateTime"`
@@ -61,4 +61,40 @@ func TampilkanBarangById(db *gorm.DB, idBarang string) ([]Barang, int) { //retur
 	idKategori := barang[0].IdKategori //mengambil idKategori dari produk terpilih untuk dijadikan default value pada dropdown
 
 	return barang, idKategori
+}
+
+// Update stok barang yang ada pada detil transaksi
+func UpdateStokBarangDetilOrder(db *gorm.DB, bdo []BarangDetilOrder) {
+	barang := Barang{}
+
+	//var idBarang int
+
+	for i := range bdo {
+		barang.ID = bdo[i].IdBarang
+		log.Println("ID barang ke : ", i+1, " :", barang.ID)
+
+		_ = db.Find(&barang, "id = ?", barang.ID) //ambil 1 row dengan ID tertentu
+
+		log.Println("Stok barang : ", barang.Stok)
+
+		barang.Stok = barang.Stok - bdo[i].Jumlah //update stok berdasarkan qty terjual
+
+		log.Println("Stok dikurangi : ", bdo[i].Jumlah)
+		log.Println("Stok barang setelah dipotong : ", barang.Stok)
+
+		_ = db.Save(&barang) //update data ke database
+		log.Println("Stok barang ", barang.ID, " telah diupdate menjadi: ", barang.Stok)
+	}
+}
+
+func TampilkanBarangSedikit(db *gorm.DB) []Barang {
+	var barang []Barang
+
+	//tampilkan barang yang stoknya < 10
+	result := db.Model(Barang{}).Preload("Kategori").Where("stok < ?", "10").Find(&barang).Error
+	if result != nil {
+		panic(result)
+	}
+
+	return barang
 }
